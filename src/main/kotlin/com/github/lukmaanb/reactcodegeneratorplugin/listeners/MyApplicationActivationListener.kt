733774317ae07e073b47002c2f47f7ffc49fb32f
GitHub Action <action@github.com>
2023-09-15 13:14:18 +0000

    Template cleanup

--- src/main/kotlin/com/github/lukmaanb/reactcodegeneratorplugin/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/lukmaanb/reactcodegeneratorplugin/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.lukmaanb.reactcodegeneratorplugin.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
